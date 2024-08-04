a6fa8c811d2822d84658437edf93c470e5bef399
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-04 15:05:31 +0300

    adding onBoardingRoute

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/on_boarding/views/on_boarding_view.dart';
 import 'package:fruit_hub/features/splash/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
+
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
+
     default:
-      return MaterialPageRoute(builder: (context)=> const Scaffold());
-
-
+      return MaterialPageRoute(builder: (context) => const Scaffold());
   }
 }
