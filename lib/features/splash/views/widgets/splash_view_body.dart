7b461865016dc0598b067583478710e4c35dfa1f
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-04 15:04:56 +0300

    splash view logic done

--- lib/features/splash/views/widgets/splash_view_body.dart
+++ lib/features/splash/views/widgets/splash_view_body.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruit_hub/core/utils/app_images.dart';
+import 'package:fruit_hub/features/on_boarding/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState(){
+    executeNavigation();
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return  Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Row(
@@ -15,9 +27,17 @@ class SplashViewBody extends StatelessWidget {
           ],
         ),
         SvgPicture.asset(Assets.imagesLogo),
-        SvgPicture.asset(Assets.imagesSplashBottom),
+        SvgPicture.asset(Assets.imagesSplashBottom,
+        fit: BoxFit.fill,),
+
 
       ],
     );
+
+  }
+  void executeNavigation(){
+    Future.delayed(const Duration(seconds:2),(){
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/helper_functions/on_generate_routes.dart';
 import 'features/splash/views/splash_view.dart';
 
-
 void main() {
   runApp(const FruitHub());
 }
@@ -16,7 +15,6 @@ class FruitHub extends StatelessWidget {
 
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
-
       debugShowCheckedModeBanner: false,
     );
   }
