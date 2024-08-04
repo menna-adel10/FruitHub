7becbd7eb33ebc3d21512906f9c59d29c51a1d65
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-04 16:30:42 +0300

    add image of page view items

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,6 +2,14 @@ class Assets{
 
   static const String imagesLogo = "assets/images/logo.svg";
 
+  static const String imagesPageViewItem1 = "assets/images/page_view_item1.svg";
+
+  static const String imagesPageViewItem2 = "assets/images/page_view_item2.svg";
+
+  static const String imagesPageViewBackground1 = "assets/images/page_view_background1.svg";
+
+  static const String imagesPageViewBackground2 = "assets/images/page_view_background2.svg";
+
   static const String imagesPlant = "assets/images/plant.svg";
 
   static const String imagesSplashBottom = "assets/images/Circles.svg";
