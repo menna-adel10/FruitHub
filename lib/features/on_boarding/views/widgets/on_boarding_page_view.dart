1e62e5eb249510f71a8df7725a5eca2df0dd3645
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-07 16:32:48 +0300

    fix issue of visibility

--- lib/features/on_boarding/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/views/widgets/on_boarding_page_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/utils/app_images.dart';
+import 'package:fruit_hub/core/utils/colors.dart';
 import 'package:fruit_hub/features/on_boarding/views/widgets/page_view_item.dart';
+
+import '../../../../core/utils/app_text_styles.dart';
 
 class OnBoardingPageView extends StatelessWidget {
   const OnBoardingPageView({super.key, required this.pageController});
@@ -13,7 +16,7 @@ class OnBoardingPageView extends StatelessWidget {
 
       children: [
         PageViewItem(
-          isVisible: (pageController.hasClients ? pageController.page!.round() : 0) == 0,
+          isVisible: true,
           image: Assets.imagesPageViewItem1,
           backgroundImage: Assets.imagesPageViewBackground1,
           subtitle:
@@ -21,14 +24,18 @@ class OnBoardingPageView extends StatelessWidget {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('مرحبًا بك في'),
-              Text('Fruit'),
-              Text('Hub'),
+              Text('مرحبًا بك في',
+              style: TextStyles.bold23,),
+              Text('HUB',
+                style: TextStyles.bold23.copyWith(color: AppColors.secondaryColor),
+              ),
+              Text('Fruit',
+                style: TextStyles.bold23.copyWith(color: AppColors.primaryColor),),
             ],
           ),
         ),
         PageViewItem(
-          isVisible: (pageController.hasClients ? pageController.page!.round() : 0)!= 0,
+          isVisible: false,
           image: Assets.imagesPageViewItem2,
           backgroundImage: Assets.imagesPageViewBackground2,
           subtitle:
@@ -44,6 +51,7 @@ class OnBoardingPageView extends StatelessWidget {
                 fontFamily: 'cairo',
                 fontWeight: FontWeight.w700,
                 height: 0,
+
               ),
               ),
             ],
