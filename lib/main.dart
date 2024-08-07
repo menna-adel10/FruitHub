24828a4520e380e4f50d4cec4e06729755ba07c6
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-07 16:32:29 +0300

    shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/helper_functions/on_generate_routes.dart';
+import 'package:fruit_hub/core/services/shared_preferences_singlton.dart';
 import 'features/splash/views/splash_view.dart';
 import 'generated/l10n.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await prefs.init();
   runApp(const FruitHub());
 }
 
@@ -14,6 +17,9 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Cairo',
+      ),
       localizationsDelegates: [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
