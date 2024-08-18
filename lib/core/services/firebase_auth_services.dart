cb1a3b84f6920770d504f0a93248620c0ee0082d
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-18 16:20:25 +0300

    add auth services and implement create user account from firebase

---
+++ lib/core/services/firebase_auth_services.dart
@@ -0,0 +1,24 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthServices {
+  Future<User?> createUserWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+}
--- lib/core/services/shared_preferences_singlton.dart
+++ lib/core/services/shared_preferences_singlton.dart
@@ -3,7 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 class prefs{
   static late SharedPreferences _instance;
 
-  //init shared preferences
   static Future<void> init()async{
     _instance = await SharedPreferences.getInstance();
 
