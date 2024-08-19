4e25993a626fd99fb3f89fc051d299d7a381aae4
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-19 14:53:04 +0300

    build get it sign up by adding service locator and bloc provider

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,13 @@
+import 'package:fruit_hub/core/services/firebase_auth_services.dart';
+import 'package:fruit_hub/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth-repo.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+  getIt.registerSingleton<AuthRepo>(AuthRepoImpl(
+    firebaseAuthServices: getIt<FirebaseAuthServices>(),
+  ));
+}
--- lib/features/auth/views/sign_up.dart
+++ lib/features/auth/views/sign_up.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/services/get_it_service.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruit_hub/features/auth/views/widgets/sign_up_view_body.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth-repo.dart';
+import '../persentation/manager/signup_cubits/signup_cubit.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
-  static const routeName='sign up';
+  static const routeName = 'sign up';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد',),
-      body: SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(
+       getIt<AuthRepo>(),
+      ),
+      child: Scaffold(
+        appBar: buildAppBar(
+          context,
+          title: 'حساب جديد',
+        ),
+        body: const SignupViewBody(),
+      ),
     );
-
   }
 }
+
