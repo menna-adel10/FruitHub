e277cf12506fd3b1239beedcc09461c5f68b9bb3
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-08-19 12:02:26 +0300

    implement auth repo impl to get the data by creating user model takes data from firebase

---
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hub/core/errors/exceptions.dart';
+import 'package:fruit_hub/core/errors/failures.dart';
+import 'package:fruit_hub/core/services/firebase_auth_services.dart';
+import 'package:fruit_hub/features/auth/data/models/user_model.dart';
+import 'package:fruit_hub/features/auth/domain/entities/user_entity.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth-repo.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthServices firebaseAuthServices;
+
+  AuthRepoImpl({required this.firebaseAuthServices});
+
+  @override
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      String email, String password, String name) async {
+    try {
+      var user = await firebaseAuthServices.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return right(UserModel.fromFireBaseUser(user));
+    } on CustomException catch (e) {
+      return left(ServerFailure(e.message));
+    } catch (e) {
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
+}
